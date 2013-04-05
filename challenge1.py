570160d7fbf4267b7d7cd9a1cad230473a1c8fd7
drizzlelabs <andre.stephens@rackspace.com>
2013-04-05 13:12:04 -0500

    Cleaned up the code a bit.

--- challenge1.py
+++ challenge1.py
@@ -1,35 +1,51 @@
-import pyrax
 import time
+import pyrax
+import argparse
 
-pyrax.set_credential_file("/Users/drizzle/Desktop/corpcreds")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('base', help="Base hostname to use for servers")
+    parser.add_argument ('-c', '--count', type=int, default=3, help="Number of servers to build. Default is 3.")
+    parser.add_argument ('-i', '--image', default='5cebb13a-f783-4f8c-8058-c4182c724ccd', help='Image ID to build server with. Default is Ubuntu 12.04')
+    parser.add_argument ('-f', '--flavor', type=int, default=2, help='Flavor ID to build server with' )
+    parser.add_argument ('-r', '--region', default='DFW', help='Datacenter/Region to build servers in', choices=['DFW', 'ORD', 'LON'])
+    
+    pyrax.set_credential_file("/Users/drizzle/Desktop/corpcreds")
+    cs = pyrax.cloudservers
+    
+    args = parser.parse_args()
+    
+    servers = {}
+    for i in xrange(0, args.count):
+        name =  '%s%d' % (args.base, i)
+        server_data = {
+            "server": {
+                "flavorRef": args.flavor,
+                "imageRef": args.image,
+                "name": name
+            }
+        }
+        servers[name] = cs.servers.create(name, args.image, args.flavor)
+        print "Building server: %s" % servers[name] 
+    
+    print 
+    
+    completed_details = []
+    while len(completed_details) < args.count:
+        for name in servers:
+            servers[name].get()
+            if len(servers[name].networks) > 0:
+                completed_details.append(servers[name])        
+        print "Waiting for details to be ready. Trying again in 30 seconds."
+        time.sleep(30)
+        
+    for name in servers:
+        servers[name].get()
+        print "Name: %s" % servers[name].name
+        print "ID: %s" % servers[name].id
+        print "Status: %s" % servers[name].status
+        print "Admin Password: %s" % servers[name].adminPass
+        print "Networks: %s \n" % servers[name].networks
 
-cs = pyrax.cloudservers
-
-# Function that invokes another function (cs.servers.create), after taking user generated input
-def serverCreator(s_prefix, s_image, s_flavor, s_quantity):
-	i = 0
-	serverDetails = {}
-	while (i < int(s_quantity)):
-		i = i + 1
-		s_name = str(s_prefix) + str(i)
-		print "Preparing Server: " + str(s_name)
-		serverDetails[s_name] = cs.servers.create(s_name, s_image, s_flavor)
-
-	print
-					
-	print "Waiting on Networking Information (60 Seconds)..."
-	time.sleep(60)
-	
-	print 
-	print
-
-	for s_name in serverDetails:
-		serverDetails[s_name].get()
-		print "### Server " + str(s_name) + " details ###"
-		print "Name:", serverDetails[s_name].name
-		print "Status:", serverDetails[s_name].status
-		print "Admin Password:", serverDetails[s_name].adminPass
-		print "Networks:", serverDetails[s_name].networks
-		print
-			
-serverCreator("web", "5cebb13a-f783-4f8c-8058-c4182c724ccd", 2, 3)
+if __name__ == '__main__':
+    main()
